3170977c5c52bae77ad6f16acc13b759270e7eb3
MeronSAssefa <meronseife6@gmail.com>
2026-05-15 15:36:47 -0700

    Ending value and lease fee logic for the lease contract

--- src/main/java/com/pluralsight/models/LeaseContract.java
+++ src/main/java/com/pluralsight/models/LeaseContract.java
@@ -1,8 +1,47 @@
 package com.pluralsight.models;
 
-public class LeaseContract {
-    public static void main(String[] args) {
+public class LeaseContract extends Contract {
+
+
+    //constructors
+    public LeaseContract(String date, String name, String email, Vehicle vehicle){
+
+        super(date, name, email, vehicle);
+    }
+
+    //Methods
+    public double getExpectedEndingValue(){
+        double price = getVehicle().getPrice();
+        double endingPrice;
+
+        endingPrice =  price * .5;
+        return endingPrice;
+    }
+
+    public double leaseFee() {
+        double price = getVehicle().getPrice();
+        double leaseFee;
+
+        leaseFee =  price * .07;
+        return leaseFee;
+
+    }
+
+
+
+
+
+    @Override
+    public void getTotalPrice(){
+
+    }
+    @Override
+    public void getMonthlyPayment(){
 
     }
 }
+
+
+
+
 
